a0bc52907c633ed7157c289523c85ef6086191a9
yanuaraditia <yanuaraditia@outlook.com>
2021-02-12 10:33:50 +0700

    Fix telegram icon

--- pages/index.vue
+++ pages/index.vue
@@ -14,8 +14,8 @@
             About Me
           </nuxt-link>
           <a class="btn btn-link btn-lg" href="//t.me/yanuaraditia">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4" /><line x1="8" y1="9" x2="16" y2="9" /><line x1="8" y1="13" x2="14" y2="13" /></svg>
-            Call me maybe
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" /></svg>
+            <span class="d-none d-lg-inline">Call me maybe</span>
           </a>
         </div>
         <div class="order-0 col-lg-auto order-lg-0">
